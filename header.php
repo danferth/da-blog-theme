00417d8eb799de090ac311ddcbf0e9eb1c76142a
Dan Klotz <danferth@gmail.com>
2017-03-07 01:29:27 +0000

    added favicons and created page spacific css and also added the old css from danfoerth.com so have something to start with

--- header.php
+++ header.php
@@ -14,7 +14,15 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/favicon.ico?v=2" type="image/x-icon">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-144x144-precomposed.png?v=3">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-114x114-precomposed.png?v=3">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-72x72-precomposed.png?v=3">
+		<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-precomposed.png?v=3">
+		
 		<?php wp_head(); ?>
+		
 	</head>
 	<body <?php body_class(); ?>>
 	<?php do_action( 'foundationpress_after_body' ); ?>
